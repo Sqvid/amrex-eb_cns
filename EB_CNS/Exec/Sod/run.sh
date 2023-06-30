454486e217b907be1bce0a0c98a9d703e47e816c
Siddhartha Menon <42359201+Sqvid@users.noreply.github.com>
2023-06-30 18:22:23 +0100

    Simplify initial state generation

--- EB_CNS/Exec/Sod/run.sh
+++ EB_CNS/Exec/Sod/run.sh
@@ -9,7 +9,7 @@ visitFile=plot.visit
 
 rm -rfv ./output
 
-make -j ${nThreads} && OMP_NUM_THREADS=1 mpiexec -n ${nCores} ${binName} inputs && rm -vf plot.visit
+make -j ${nThreads} && OMP_NUM_THREADS=1 mpiexec -n ${nCores} ${binName} inputs $@ && rm -vf plot.visit
 
 for dir in $(ls -1v --color=never ./output | sed '/.*\(old\)/d')
 do
